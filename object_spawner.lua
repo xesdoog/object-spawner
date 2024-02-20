7ffcb899c3bfab0fe5050452de05018045a95c62
SAMURAI <66764345+xesdoog@users.noreply.github.com>
2024-02-20 11:22:26 +0100

    Update object_spawner.lua
    Fixed an endless loop. No more real-time direction changing for now until I figure it out.

--- object_spawner.lua
+++ object_spawner.lua
@@ -49,13 +49,15 @@ local props = {
     { hash = 3962399788, 	name = "NSFW Ragdoll"}
 }
 local prop_index = 1
-local offset = 0
+local h_offset = 0
+local default_h_offset = 0
 local spawnDistance = { x = 0, y = 0, z = -1 }
 local defaultSpawnDistance = { x = 0, y = 0, z = -1 }
-local function resetDistanceSliders()
+local function resetSliders()
     spawnDistance.x = defaultSpawnDistance.x
     spawnDistance.y = defaultSpawnDistance.y
     spawnDistance.z = defaultSpawnDistance.z
+    h_offset = default_h_offset
 end
 
 object_spawner:add_text("Select an Object")
@@ -102,7 +104,7 @@ object_spawner:add_imgui(displayFilteredList)
 
 object_spawner:add_separator()
 
-object_spawner:add_text("Choose Spawning Distance or Leave Default :")
+object_spawner:add_text("Adjust Distance or Keep Default :")
 
 object_spawner:add_imgui(function()
     spawnDistance.x, _ = ImGui.SliderFloat("X Axis", spawnDistance.x, -10, 10)
@@ -110,25 +112,21 @@ object_spawner:add_imgui(function()
     spawnDistance.z, _ = ImGui.SliderFloat("Z Axis", spawnDistance.z, -10, 10)
 end)
 
+object_spawner:add_separator()
+
+object_spawner:add_text("Adjust Direction or Keep Default :")
+
+object_spawner:add_imgui(function()
+h_offset, _ = ImGui.SliderFloat("Heading Offset", h_offset, 0, 360)
+end)
+
 defaultSpawnDistance.x = spawnDistance.x
 defaultSpawnDistance.y = spawnDistance.y
 defaultSpawnDistance.z = spawnDistance.z
+default_h_offset = h_offset
 
 object_spawner:add_button("Reset Sliders", function()
-    resetDistanceSliders()
-end)
-
-object_spawner:add_separator()
-
-object_spawner:add_text("Choose Facing Direction or Leave Default :")
-
-object_spawner:add_imgui(function()
-offset, isChanged = ImGui.InputInt("Facing Direction", offset, 15, 90, 0)
-end)
-
-object_spawner:add_button("Reset Direction", function()
-    offset = 0
-    isChanged = false
+    resetSliders()
 end)
 
 object_spawner:add_separator()
@@ -141,24 +139,23 @@ coords.x = coords.x + spawnDistance.x
 coords.y = coords.y + spawnDistance.y
 coords.z = coords.z + spawnDistance.z
 local heading = ENTITY.GET_ENTITY_HEADING(ped)
+heading = heading + h_offset
 local forwardX = ENTITY.GET_ENTITY_FORWARD_X(ped)
 local forwardY = ENTITY.GET_ENTITY_FORWARD_Y(ped)
 local object = filteredItems[prop_index+1]
     if ImGui.Button("Spawn Object") then
         script.run_in_fiber(function()
-                if object then
-                    while not STREAMING.HAS_MODEL_LOADED(object.hash) do
-                        STREAMING.REQUEST_MODEL(object.hash)
-                        coroutine.yield()
-                    end
-                end
+        if object then
+            while not STREAMING.HAS_MODEL_LOADED(object.hash) do
+                STREAMING.REQUEST_MODEL(object.hash)
+                coroutine.yield()
+            end
+            end
             local prop = OBJECT.CREATE_OBJECT(object.hash, coords.x + (forwardX * 1), coords.y + (forwardY * 1), coords.z, true, false, false)
-            script.register_looped("change direction", function()
-                if isChanged then
-                ENTITY.SET_ENTITY_HEADING(prop, heading + offset)
-                end
-            end)
-                OBJECT.PLACE_OBJECT_ON_GROUND_PROPERLY(prop)
+            ENTITY.SET_ENTITY_HEADING(prop, heading)
+            OBJECT.PLACE_OBJECT_ON_GROUND_PROPERLY(prop)
+            local netID = NETWORK.OBJ_TO_NET(prop)
+            NETWORK.SET_NETWORK_ID_EXISTS_ON_ALL_MACHINES(netID, true)
                 if ENTITY.DOES_ENTITY_EXIST(prop) then
                     gui.show_message("Object Spawner", "Spawned '"..object.name.."' in front of ["..player_name.."].")
                 else
